4f4994b428a66e146d6ccb8e1505ac257a4bfa4e
Violetta Iuzykhovich <violetta100100100@gmail.com>
2022-09-28 20:40:54 +0300

    Init commit from console

---
+++ src/test/java/org/veta/ExampleTest.java
@@ -0,0 +1,16 @@
+package org.veta;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+    @Test
+    void exampleTest0() {
+        Assertions.assertFalse(2 <= 1);
+    }
+
+    @Test
+    void exampleTest1() {
+        Assertions.assertTrue(3 >= 2);
+    }
+}
