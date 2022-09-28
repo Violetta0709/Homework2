dcdfd511ecf1b60dda24771f799a1d7fb78aa222
Violetta0709 <107357798+Violetta0709@users.noreply.github.com>
2022-09-28 21:44:38 +0300

    Update ExampleTest.java

--- src/test/java/org/veta/ExampleTest.java
+++ src/test/java/org/veta/ExampleTest.java
@@ -8,16 +8,16 @@ import java.util.Random;
 public class ExampleTest {
     @Test
     void exampleTest0() {
-        Assertions.assertFalse(2 < 1);
+        Assertions.assertTrue(2 < 1);
     }
 
     @Test
     void exampleTest1() {
-        Assertions.assertTrue(3 > 2);
+        Assertions.assertFalse(3 > 2);
     }
     @Test
     void exampleTest2() {
-        Assertions.assertTrue(4 > 2);
+        Assertions.assertFalse(4 > 2);
     
     }
 }
