6745166821a1846701de9f5683127560193d99fd
Violetta Iuzykhovich <violetta100100100@gmail.com>
2022-09-28 21:42:47 +0300

    another conflict

--- src/test/java/org/veta/ExampleTest.java
+++ src/test/java/org/veta/ExampleTest.java
@@ -8,16 +8,16 @@ import java.util.Random;
 public class ExampleTest {
     @Test
     void exampleTest0() {
-        Assertions.assertFalse(2 < 1);
+        Assertions.assertFalse(2 < 4);
     }
 
     @Test
     void exampleTest1() {
-        Assertions.assertTrue(3 > 2);
+        Assertions.assertTrue(3 > 4);
     }
     @Test
     void exampleTest2() {
-        Assertions.assertTrue(4 > 2);
+        Assertions.assertTrue(4 > 5);
     
     }
 }
