8a4e58ea6b15a3ded6469a5e36021a845b2bb8b3
Violetta Iuzykhovich <violetta100100100@gmail.com>
2022-10-02 18:06:34 +0300

    changes fixlogic3

--- src/test/java/org/veta/ExampleTest.java
+++ src/test/java/org/veta/ExampleTest.java
@@ -8,16 +8,16 @@ import java.util.Random;
 public class ExampleTest {
     @Test
     void exampleTest0() {
-        Assertions.assertTrue(2 < 1);
+        Assertions.assertTrue(2 < 8);
     }
 
     @Test
     void exampleTest1() {
-        Assertions.assertFalse(3 > 5);
+        Assertions.assertFalse(3 > 9);
     }
     @Test
     void exampleTest2() {
-        Assertions.assertFalse(4 > 2);
-    
+        Assertions.assertFalse(4 > 10);
+
     }
 }
